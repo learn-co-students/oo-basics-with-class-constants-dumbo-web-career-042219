29f8e45af860f49b651f88ff501c93a5263a3a2f
Chloe <gingkolane@gmail.com>
2019-04-24 12:43:31 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,31 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand) != true
+      BRANDS << brand
+    end
+
+  end
+
+
+# this is a writer for brand, so it is brand=
+# this situation is brand variable is a class constant, meaning it has a valuelist, and the valuelist is in BRANDS
+# because it is a special writer, so we have to define the writer separately, instead of just using attr_writer or attr_accessor
+  #
+
+  BRANDS = []
+  def BRANDS=()
+    @BRANDS = brands
+    BRANDS << brands.unique
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 
 end
